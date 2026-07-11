ad9daca3f16380163a41f69e4bc93fdca63a8e86
web8898-web <web8898@gmail.com>
2026-07-12 04:16:32 +0800

    Enable duplicate task action removal

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ import TaskModeTutorial from "@/components/TaskModeTutorial";
 import TaskModeCopyFix from "@/components/TaskModeCopyFix";
 import TaskListSpacingFix from "@/components/TaskListSpacingFix";
 import TaskTabsScrollFix from "@/components/TaskTabsScrollFix";
+import TaskListBottomActionFix from "@/components/TaskListBottomActionFix";
 
 const APP_TITLE = "接龍報名小助手";
 const APP_DESCRIPTION = "在 LINE 上分享、免登入即可完成報名的接龍小工具";
@@ -108,6 +109,7 @@ export default function RootLayout({ children }) {
         <TaskModeCopyFix />
         <TaskListSpacingFix />
         <TaskTabsScrollFix />
+        <TaskListBottomActionFix />
         <div className="w-full max-w-md mx-auto min-h-screen bg-white shadow-xl relative flex flex-col">
           {children}
         </div>
