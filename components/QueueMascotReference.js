9998e6d805cdf1d439d220fc3a904111d929c7cd
web8898-web <web8898@gmail.com>
2026-07-11 11:31:09 +0800

    Fix mascot resting arms for phone and watch actions

--- components/QueueMascotReference.js
+++ components/QueueMascotReference.js
@@ -40,9 +40,9 @@ function ensureStyles() {
     .queue-reference-normal-mouth{opacity:1}
     .queue-reference-rest-arm{opacity:1;transition:opacity 80ms ease}
 
-    .queue-reference-phone .queue-reference-rest-arm-left{opacity:0}
-    .queue-reference-yawn .queue-reference-rest-arm-right,
-    .queue-reference-watch .queue-reference-rest-arm-right{opacity:0}
+    .queue-reference-phone .queue-reference-rest-arm{opacity:0}
+    .queue-reference-yawn .queue-reference-rest-arm-right{opacity:0}
+    .queue-reference-watch .queue-reference-rest-arm-left{opacity:0}
     .queue-reference-stretch .queue-reference-rest-arm,
     .queue-reference-drink .queue-reference-rest-arm,
     .queue-reference-read .queue-reference-rest-arm{opacity:0}
